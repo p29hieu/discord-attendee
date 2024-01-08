0ff853b1b861b35860c6e2193bcb2c1aba9d00fa
hieu <hieu@192.168.1.34>
2024-01-08 17:56:54 +0000

    init

---
+++ src/index.ts
@@ -0,0 +1,119 @@
+import axios from "axios";
+import fs from "fs/promises";
+
+import config from "../config.json";
+
+const ONE_DAY_MS = 24 * 3600 * 1000;
+const ONE_MINUTE_MS = 60 * 1000;
+
+function generateRandomNumberString(length: number) {
+  let result = "";
+  const characters = "0123456789";
+  const charactersLength = characters.length;
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * charactersLength);
+    result += characters.charAt(randomIndex);
+  }
+
+  return result;
+}
+
+const getAuthorizationKey = async () => {
+  return await fs.readFile("./AuthorizationKey", "utf-8");
+};
+
+const init = async () => {
+  // create result file
+  await fs.appendFile("./log.txt", ``);
+  await fs.appendFile("./error.txt", ``);
+};
+
+const addLog = async (log: any) => {
+  await fs.appendFile(
+    "./log.txt",
+    `${new Date().toISOString()}: ${JSON.stringify(log)} \n`
+  );
+};
+
+const addError = async (error: any) => {
+  await fs.appendFile(
+    "./error.txt",
+    `${new Date().toISOString()}: ${JSON.stringify(error)}\n`
+  );
+};
+
+const updateConfig = async () => {
+  config.lastSuccessTime = new Date().toISOString();
+  await fs.writeFile("config.json", JSON.stringify(config, null, 2));
+};
+
+const attendee = async ({
+  updateSuccessTime = false,
+}: {
+  updateSuccessTime?: boolean;
+}) => {
+  try {
+    const data = await axios
+      .post(
+        config.channel,
+        {
+          mobile_network_type: "unknown",
+          content: "!daily",
+          nonce: generateRandomNumberString(19),
+          tts: false,
+          flags: 0,
+        },
+        {
+          headers: {
+            accept: "*/*",
+            authorization: await getAuthorizationKey(),
+            "content-type": "application/json",
+          },
+        }
+      )
+      .then(async (r) => {
+        return r.data;
+      });
+    if (updateSuccessTime) {
+      await updateConfig();
+    }
+    await addLog(data);
+  } catch (error: any) {
+    if (axios.isAxiosError(error)) {
+      await addError(error.response?.data);
+    }
+  }
+};
+
+const cron = async () => {
+  const lastSuccessTime = new Date(config.lastSuccessTime).getTime();
+  const diff = ONE_DAY_MS - (Date.now() - lastSuccessTime);
+
+  for (let i = 0; i < config.totalMinute; i++) {
+    for (let j = 0; j < config.numberRequestPerMinute; j++) {
+      const timeout =
+        diff +
+        ONE_MINUTE_MS * i +
+        j * Math.floor(ONE_MINUTE_MS / config.numberRequestPerMinute) +
+        config.timeoutBetweenAttendee;
+      console.log(
+        "===next message will be send at: ",
+        new Date(Date.now() + timeout)
+      );
+      setTimeout(() => {
+        attendee({ updateSuccessTime: i === 0 && j === 0 });
+      }, timeout);
+    }
+  }
+};
+
+const main = async () => {
+  await init();
+  cron();
+  setInterval(() => {
+    cron();
+  }, ONE_DAY_MS);
+};
+
+main();
